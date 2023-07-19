e98056c5fa38198c36cbcbdaacf6535d331ad54c
jj0308 <jurejukic999@gmail.com>
2023-07-19 10:46:51 +0200

    Fixed Connection

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGO_URL = process.env.DB_CONNECTION_STRING;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
